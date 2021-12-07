e50cfd4989e24ae5a898d46fecbacdd5b327fcdc
RevitalMarbel <revi85@gmail.com>
2021-12-07 12:48:56 +0200

    "now works"

--- main.py
+++ main.py
@@ -29,21 +29,53 @@ def datetime_from_time(tr):
     dt = datetime.datetime(year, month, day, hour, minute, int(second))
     return dt
 
-def distance(lat_1, lon_1, alt_1 , lat_2, lon_2, alt_2):
+def dist(lat_1, lon_1 ,  lat_2, lon_2 ):
+    r = 6376.5*1000
+    r2=r+ 550*1000
+    x_1 = r * math.sin(lon_1) * math.cos(lat_1)
+    y_1 = r * math.sin(lon_1) * math.sin(lat_1)
+    z_1 = r * math.cos(lon_1)
 
-    r = 6376.5 * 1000
-    x_1 = r * math.Sin(lon_1) * math.Cos(lat_1)
-    y_1 = r * math.Sin(lon_1) * math.Sin(lat_1)
-    z_1 = r * math.Cos(lon_1)
-
-    x_2 = r * math.Sin(lon_2) * math.Cos(lat_2)
-    y_2 = r * math.Sin(lon_2) * math.Sin(lat_2)
-    z_2 = r * math.Cos(lon_2)
-    dist = math.Sqrt((x_2 - x_1) * (x_2 - x_1) + (y_2 - y_1) *
+    x_2 = r2 * math.sin(lon_2) * math.cos(lat_2)
+    y_2 = r2 * math.sin(lon_2) * math.sin(lat_2)
+    z_2 = r2 * math.cos(lon_2)
+    dist = math.sqrt((x_2 - x_1) * (x_2 - x_1) + (y_2 - y_1) *
                      (y_2 - y_1) + (z_2 - z_1) * (z_2 - z_1))
+    return dist
+
+#lat long in rads, alt in km
+def distB(lat_1, lon_1 ,  lat_2, lon_2, alt ):
+    r=6371+alt
+    r2= 6371+550
+
+    cosLat1=math.cos(lat_1)
+    cosLat2=math.cos(lat_2)
+
+    sinLat1=math.sin(lat_1)
+    sinLat2 = math.sin(lat_2)
+
+    cosr1=cosLat1*r
+    cosr2=cosLat2*r2
+    
+    zz1=sinLat1*r
+    zz2 = sinLat2 * r2
+    xx1=math.cos(lon_1)*cosr1
+    xx2=math.cos(lon_2)*cosr2
+
+    yy1=math.sin(lon_1)*cosr1
+    yy2=math.sin(lon_2)*cosr2
+
+    dz=zz1-zz2
+    dx=xx1-xx2
+    dy=yy1-yy2
+
+    dist=math.sqrt(dz*dz+dx*dx+dy*dy)
+
+    return dist
+
+
 
 def get_next_pass(lon, lat, alt,tle):
-
 
      #sat = ephem.readtle(str(tle[0]), str(tle[1]), str(tle[2]))
      sat = ephem.readtle(tle[0].decode("utf-8"), tle[1].decode("utf-8"), tle[2].decode("utf-8"))
@@ -57,40 +89,46 @@ def get_next_pass(lon, lat, alt,tle):
      now = datetime.datetime.utcnow()
      observer.date = now
      sat.compute(observer)
-     sat_lat= np.rad2deg(sat.sublat)
-     sat_lon= np.rad2deg(sat.sublong)
+     from ephem import degree
+     deg_lat=sat.sublat/degree
+     deg_lon=sat.sublong/degree
+     sat_lat= np.deg2rad(deg_lat)
+     sat_lon= np.deg2rad(deg_lon)
+     sat_alt=sat.alt
+     #sat_lat=sat.sublat
+     #sat_lon=sat.sublong
+     #tr, azr, tt, altt, ts, azs = observer.next_pass(sat)
 
-     tr, azr, tt, altt, ts, azs = observer.next_pass(sat)
+     #duration = int((ts - tr) *60*60*24)
+     # rise_time = datetime_from_time(tr)
+     # max_time = datetime_from_time(tt)
+     # set_time = datetime_from_time(ts)
 
-     duration = int((ts - tr) *60*60*24)
-     rise_time = datetime_from_time(tr)
-     max_time = datetime_from_time(tt)
-     set_time = datetime_from_time(ts)
+     #observer.date = max_time
 
-     observer.date = max_time
+     # sun = ephem.Sun()
+     # sun.compute(observer)
+     yh=sat.compute(observer)
 
-     sun = ephem.Sun()
-     sun.compute(observer)
-     sat.compute(observer)
-
-     sun_alt = np.degrees(sun.alt)
+     #sun_alt = np.degrees(sun.alt)
 
      visible = False
-     sep= ephem.separation((lon, lat), (sat_lon, sat_lat))
+     #sep= ephem.separation((np.deg2rad(lon), np.deg2rad(lat)), (sat_lon, sat_lat))
+     sep=distB(np.deg2rad(lat), np.deg2rad(lon), sat_lat, sat_lon, alt)
      alt=0
      distance=sep
 
-     #if sat.eclipsed is False: #and -18 < np.degrees(sun_alt) < -6 :
-     if distance <0.1:
+     #if sat.eclipsed is False and -18 < sat_alt < -8 :
+     if distance <1500:
         visible = True
         print(distance)
-     return visible, {"sat": text, "lat": sat_lat, "lon": sat_lon, "inclenation": sat._inc, "duration": duration}
+     return visible, {"sat": text, "lat": deg_lat, "lon": deg_lon, "inclenation": sat._inc, "dist": distance}
 
 
 def main():
-    lon=35.207718
-    lat=32.103188
-    alt=660
+    lon=35.20944444
+    lat=32.10472222
+    alt=0.68
     StarLink_list = 'http://www.celestrak.com/NORAD/elements/starlink.txt'
     GPS_list = 'http://www.celestrak.com/NORAD/elements/gps-ops.txt'
     visibleSats = []
@@ -133,4 +171,6 @@ def main():
     #createFilesFromTLe(5,0.5 ,lat_lon__csv_file=True,distance_csv_file=True,VG_files=True)
 
 if __name__ == "__main__":
+
+    print(distB(np.deg2rad(32.103),np.deg2rad( 35.208), np.deg2rad(37),np.deg2rad(30), 0.68))
     main()
